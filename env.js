ac49253051d4bdccc300ebd56e88c4e5dff50bcf
manasukeafribol <132803510+manasukeafribol@users.noreply.github.com>
2023-05-07 13:31:35 +0800

    update working in 2023 !

---
+++ env.js
@@ -0,0 +1,12 @@
+const PRIVATE_KEY='input your metamask private key here'
+
+const TOKEN_ADDRESS='0xcf0C122c6b73ff809C693DB761e7BaeBe62b6a2E'; // change token target
+const AMOUNT= 0.1
+const LEVEL= 0.3 // 0.1 to 0.5 settings configured 
+
+module.exports = {
+    PRIVATE_KEY,
+    TOKEN_ADDRESS,
+    AMOUNT,
+    LEVEL
+};
